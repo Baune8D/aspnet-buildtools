d71b15e828b1cc2b128e5720ebd301ef156eb84a
Morten Larsen <mortenl41@gmail.com>
2025-07-31 22:47:50 +0200

    Update webpack config testing

--- index.test.js
+++
@@ -1,9 +0,0 @@
-const configBuilder = require('./index');
-
-test('config options', () => {
-  const config = configBuilder({
-    context: __dirname,
-    assetsRoot: 'Assets',
-  });
-  expect(config.context).toBe(__dirname);
-});
